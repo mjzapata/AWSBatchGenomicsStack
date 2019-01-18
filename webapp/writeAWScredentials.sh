af5bd0f7dd9caa76f70af7a746928914ee31d5b6
mjzapata <malcolmzapata@gmail.com>
2019-01-18 13:58:37 -0500

    updated unlocking and locking aws permissions file for writing

--- webapp/writeAWScredentials.sh
+++ webapp/writeAWScredentials.sh
@@ -7,8 +7,6 @@
 #AWSACCESSKEYID=mysecretkeyid
 #AWSSECRETACCESSKEY=mysecretkey
 
-git clone https://github.com/mjzapata/BLJBatchAWS.git
-
 if [ $# -eq 5 ]; then
 	PROFILE=$1
 	REGION=$2
@@ -17,17 +15,24 @@ if [ $# -eq 5 ]; then
 	AWSSECRETACCESSKEY=$5
 
 	mkdir -p ~/.aws
+	
+	#unlock the files
+	chmod -R 777 ~/.aws/*
+
 	echo -e "[${PROFILE}]\noutput = ${OUTPUTFORMAT}\nregion = ${REGION}" > ~/.aws/config
 	echo -e "[${PROFILE}]\naws_access_key_id = ${AWSACCESSKEYID}\naws_secret_access_key = ${AWSSECRETACCESSKEY}" > ~/.aws/credentials
 
-	chmod 600 ~/.aws/config
-	chmod 600 ~/.aws/credentials
+	#relock the files
+	chmod -R 600 ~/.aws/*
+
 
 else
 
 
-	echo "Usage: ./writeAWScredentials.sh PROFILE REGION OUTPUTFORMAT AWSACCESSKEYID AWSSECRETACCESSKEY"
+	echo "Usage: ./writeAWScredentials.sh PROFILE REGION OUTPUTFORMAT AWSACCESSKEYID AWSSECRETACCESSKEY "
 fi
 
 
-./writeAWScredentials.sh $PROFILE $REGION $OUTPUTFORMAT $AWSACCESSKEYID $AWSSECRETACCESSKEY
+#./writeAWScredentials.sh $PROFILE $REGION $OUTPUTFORMAT $AWSACCESSKEYID $AWSSECRETACCESSKEY
+
+
