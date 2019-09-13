ab6a7f27bd550036e997f5833d0ddf2d6e0e3121
mjzapata <malcolmzapata@gmail.com>
2019-09-12 21:17:46 -0400

    documentation

---
+++ revproxy/readme.md
@@ -0,0 +1,15 @@
+
+
+## edit the .env file
+
+## start.sh new
+
+
+
+## to join a new container to the network use:
+# 1.) add the container to service definitions, docker-compose 
+  and then restart the app
+# 2.) docker run ..... --network revproxy
+ add the specifications to service definitions
+ and re-run generate nginx config and restart nginx
+ #docker restart revproxy_nginx_1
--- revproxy/start.sh
+++ revproxy/start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#edit the .env file
-
-#to join a new container to the network use:
-# 1.) add the container to service definitions, docker-compose 
-#  and then restart the app
-# 2.) docker run ..... --network revproxy
-# add the specifications to service definitions
-# and re-run generate nginx config and restart nginx
-# #docker restart revproxy_nginx_1
 set -e
 
 function print_help {
